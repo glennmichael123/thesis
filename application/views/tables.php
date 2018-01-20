704a28bc021a10ea2c82c42ce001db529d17d630
bondjames1212 <bondjames1212@gmail.com>
2018-01-20 15:31:34 +0800

    gwapo batig nawng atag.iya

--- application/views/tables.php
+++ application/views/tables.php
@@ -9,8 +9,8 @@
                                     <th>Name</th>
                                     <th>Course & Year</th>
                                     <th>School Year</th>
-                                    <th>Evaluations [ OJT 1 ]</th>
-                                    <th>Evaluations [ OJT 2 ]</th>
+                                    <th>Evaluations</th>
+                              
                                     <th>Status</th>
                                 </tr>
                             </thead>
@@ -24,8 +24,8 @@
                                             <th>Name</th>
                                             <th>Course & Year</th>
                                             <th>School Year</th>
-                                            <th>Evaluations [ OJT 1 ]</th>
-                                            <th>Evaluations [ OJT 2 ]</th>
+                                            <th>Evaluations</th>
+                                            
                                             <th>Status</th>
                                         </tr>
                                     </thead>
@@ -34,7 +34,7 @@
                                       <?php foreach($student_list as $student):?>
                                           <tr class="dashTable">
                                               <td style="text-align: center;width: 45px"><input type="checkbox" class="checkitem" value="<?php echo $student['id_number']?>" name="usernames[]"></td>
-                                              <td><a href="studentinfo/<?php echo $student['id_number']?>"><?php echo $student['last_name'] . ", " . $student['first_name']?></a></td>
+                                              <td><a href="studentinfo/<?php echo $student['id_number']?>" target="_blank"><?php echo $student['last_name'] . ", " . $student['first_name']?></a></td>
                                               <td><?php echo $student['course']." - ".$student['year']?></td>
                                               <td><?php echo $student['school_year']?></td>
                                               <td>
@@ -52,20 +52,7 @@
                                                 <?php endif; ?>
                                               </td>
 
-                                              <td>
-                                                <?php if ($student['ojtone_current_evaluations'] == 1 || $student['ojtone_current_evaluations'] == 2 || $student['ojttwo_current_evaluations'] == 1 || $student['ojttwo_current_evaluations'] == 2): ?>
-                                                  <a href="<?php base_url() ?>viewmidterm/<?php echo $student['id_number']; ?>" target="_blank">
-                                                    Midterm <i class="fa fa-check-circle"></i></a>
-                                                <?php else:?>
-                                                  <a style="color:gray">Midterm <i class="fa fa-times-circle"></i></a>
-                                                <?php endif;?>  
-
-                                                <?php if ($student['ojtone_current_evaluations'] == 2 || $student['ojttwo_current_evaluations'] == 2): ?>
-                                                    | <a target="_blank" href="<?php base_url() ?>viewfinal/<?php echo $student['id_number']; ?>"> Final <i class="fa fa-check-circle"></i></a> 
-                                                <?php else: ?>
-                                                    | <a style="color: gray">Final <i class="fa fa-times-circle"></i></a> 
-                                                <?php endif; ?>
-                                              </td>
+                                              
                                    
                                               <?php if ($student['ojtone_rendered'] >= $student['ojtone_required'] && $student['ojtone_current_evaluations'] == 2):?>
                                                   <td style="color:green;">OJT-1 Completed</td>
